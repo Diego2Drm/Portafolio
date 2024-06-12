95ca644346ff451785ad363071d41d6b41a1616c
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-06-12 16:13:48 -0600

    Project Initialization and Tailwind Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "Background" : "#081B29",
+        "Background2" : "#1C5E8F",
+        "Text" : "#00ABF0",
+        "action" : "#00EEFF",
+        "text2" : "#1DE5FF",
+        "text3" : "#118999",
+      },
+      fontFamily: {
+        "Poppins": [ "Poppins", " sans-serif"] ,
+      }
+    },
+  },
+  plugins: [],
+}
+
