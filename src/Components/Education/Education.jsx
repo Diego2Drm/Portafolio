e0634c1159364dc67645852a8fde55bee84e0fbd
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-10-14 22:27:34 -0600

    Framer Motion Education

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,36 +1,49 @@
 import React, { useContext, useState } from "react";
 import { Modal } from "./Modal";
-import { CertificatesContext } from "./Context";
-
+import { DataContext } from "../../Context/Context";
+import { motion } from "framer-motion";
 
 function Education() {
   const [open, setOpen] = useState(false);
-  const context = useContext(CertificatesContext);
+  const context = useContext(DataContext);
   const showCertificates = (url) => {
     setOpen(!open);
-    context.setCertificateImg(url)
+    context.setCertificateImg(url);
   };
 
   return (
-    <section id="Education" className="w-full flex flex-col items-center gap-10 min-h-screen mb-10 lg:pl-64 lg:items-start xl:pl-96">
-      <h2 className="text-blue-500 font-bold text-3xl tracking-[5px]">Educación</h2>
+    <section
+      id="Education"
+      className="w-full flex flex-col items-center gap-10 min-h-screen mb-10 lg:pl-64 lg:items-start xl:pl-96"
+    >
+      <h2 className="text-blue-500 font-bold text-3xl tracking-[5px]">
+        Educación
+      </h2>
 
-      <div className="lg:flex flex-wrap gap-5 lg:justify-center xl:gap-32 xl:mt-5 ">
-        {
-          context.imgUrl.map(link => (
-            <section key={link.id}>
-              <figure className="w-80 mb-5 border-2 border-blue-500 rounded-3xl overflow-hidden"
-                onClick={() => showCertificates(link.url)}>
-                <img className="rounded-3xl hover:scale-125 cursor-pointer" src={link.url} alt="certificate" />
-              </figure>
+      <div className="lg:flex flex-wrap gap-5 xl:mt-5 ">
+        {context.imgUrl.map((link) => (
+          <section key={link.id}>
+            <motion.figure
+              className="w-80 mb-5 border-2 border-blue-500 rounded-3xl overflow-hidden"
+              onClick={() => showCertificates(link.url)}
+              initial={{opacity: 0}}
+              whileInView={{opacity: 1, transition: {
+                duration: 1, delay: link.id / 4
+              }}}
+            >
+              <img
+                className="rounded-3xl duration-500 hover:scale-125 cursor-pointer"
+                src={link.url}
+                alt="certificate"
+              />
+            </motion.figure>
 
-              <Modal open={open} onclose={() => setOpen(false)} />
-            </section>
-          ))
-        }
+            <Modal open={open} onclose={() => setOpen(false)} />
+          </section>
+        ))}
       </div>
     </section>
   );
-};
+}
 
 export { Education };
--- src/Components/Education/Modal.jsx
+++ src/Components/Education/Modal.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from "react";
-import { CertificatesContext } from "./Context";
+import { DataContext } from "../../Context/Context";
 
 function Modal({ open, onclose }) {
 
-  const context = useContext(CertificatesContext)
+  const context = useContext(DataContext)
 
   return (
-      <section className={`fixed flex flex-col z-10 inset-0 justify-center items-center transition-colors ${open ? "visible bg-black/80" : "invisible"}`}>
-        <div className="text-6xl text-white cursor-pointer flex mb-8"
+      <section className={`fixed flex flex-col z-10 inset-0 justify-center items-center transition-colors ${open ? "visible bg-black/25" : "invisible"}`}>
+        <div className="text-6xl text-white cursor-pointer flex mb-8 hover:text-red-500"
           onClick={onclose}>
           <ion-icon name="close-circle-outline"></ion-icon>
         </div>
-        <img className="lg:w-[800px] xl:w-[950px]" src={context.certificateImg} alt="" />
+        <img className="md:w-[550px] lg:w-[600px] rounded-xl" src={context.certificateImg} alt="" />
       </section>
   )
 }
