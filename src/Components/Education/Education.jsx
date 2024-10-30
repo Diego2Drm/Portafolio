70cbc7c6cee64170ebfdbf22cb10f40b546a124f
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-10-29 22:58:27 -0600

    Framer Motion h2, viewport

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Modal } from "./Modal";
 import { DataContext } from "../../Context/Context";
 import { motion } from "framer-motion";
+import { Element } from "react-scroll";
 
 function Education() {
   const [open, setOpen] = useState(false);
@@ -14,15 +15,15 @@ function Education() {
   return (
     <section
       id="Education"
-      className="w-full flex flex-col items-center gap-10 min-h-screen mb-10 lg:pl-64 lg:items-start xl:pl-96"
+      className="w-full flex flex-col p-5 gap-10 min-h-screen mb-10 lg:pl-64 lg:items-start"
     >
-      <h2 className="text-blue-500 font-bold text-3xl tracking-[5px]">
+      <motion.h2 initial={{opacity: 0, x: -60}} whileInView={{opacity: 1, x: 0, transition: { duration: 1, delay: 1}}} viewport={{once: true, amount: 0.8}} className="text-blue-500 font-bold text-3xl tracking-[5px]">
         Educación
-      </h2>
+      </motion.h2>
 
-      <div className="lg:flex flex-wrap gap-5 xl:mt-5 ">
+      <article className="flex flex-wrap gap-10 xl:mt-5">
         {context.imgUrl.map((link) => (
-          <section key={link.id}>
+          <div className="w-80" key={link.id}>
             <motion.figure
               className="w-80 mb-5 border-2 border-blue-500 rounded-3xl overflow-hidden"
               onClick={() => showCertificates(link.url)}
@@ -30,6 +31,7 @@ function Education() {
               whileInView={{opacity: 1, transition: {
                 duration: 1, delay: link.id / 4
               }}}
+              viewport={{once: true, amount: 0.8}}
             >
               <img
                 className="rounded-3xl duration-500 hover:scale-125 cursor-pointer"
@@ -39,9 +41,9 @@ function Education() {
             </motion.figure>
 
             <Modal open={open} onclose={() => setOpen(false)} />
-          </section>
+          </div>
         ))}
-      </div>
+      </article>
     </section>
   );
 }
